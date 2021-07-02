fcd19fde85b6b83c810a1a4908c8757f2c0ac3fd
InHwan Jung <inhwan.jung@gmail.com>
2021-07-02 21:54:21 +0900

    July2, 2021 updated

--- services/mysql/init/setup.sql
+++ services/mysql/init/setup.sql
@@ -1,24 +1,29 @@
 DROP DATABASE IF EXISTS `pythonDB`;
-CREATE DATABASE IF NOT EXISTS `pythonDB` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `pythonDB`;
+DROP DATABASE IF EXISTS `stockDB`;
+CREATE DATABASE IF NOT EXISTS `stockDB` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `stockDB`;
 
-DROP TABLE IF EXISTS `tbl_crawlingdata`;
-CREATE TABLE IF NOT EXISTS `tbl_crawlingdata` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(50) DEFAULT NULL,
-  `price` varchar(50) DEFAULT NULL,
-  `area` varchar(50) DEFAULT NULL,
-  `contents` text DEFAULT NULL,
-  `keyword` varchar(50) DEFAULT NULL,
-  `regdate` timestamp NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS `company_info`;
+CREATE TABLE IF NOT EXISTS company_info (
+    code VARCHAR(20),
+    company VARCHAR(40),
+    last_update DATE,
+    PRIMARY KEY (code)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `tbl_keyword`;
-CREATE TABLE IF NOT EXISTS `tbl_keyword` (
-  `keyword` varchar(50) NOT NULL,
-  PRIMARY KEY (`keyword`)
+DROP TABLE IF EXISTS `daily_price`;
+CREATE TABLE IF NOT EXISTS daily_price (
+    code VARCHAR(20),
+    date DATE,
+    open BIGINT(20),
+    high BIGINT(20),
+    low BIGINT(20),
+    close BIGINT(20),
+    diff BIGINT(20),
+    volume BIGINT(20),
+    PRIMARY KEY (code, date)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 SET @@global.sql_mode= ‘’;
+SET GLOBAL local_infile = 1;
 
